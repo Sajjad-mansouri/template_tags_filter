6fc9f491cbfa66ad79dece97d8c8a2abfbadd3e1
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-07-08 18:37:52 +0330

    create model,urls and views to display post list and detail

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Post(models.Model):
+	title=models.CharField(max_length=100)
+	slug=models.SlugField(max_length=100,unique=True)
+	author=models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+	text=models.TextField()
+	created=models.DateTimeField(auto_now_add=True)
+	updated=models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.title
+
+
