7ded356835e2f6f82b6d505e2a894976c192798b
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-07-09 13:10:36 +0330

    add author field automatically to model with manipulate form_valid

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView,CreateView
 from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
 from .models import Post
 
 class PostList(ListView):
@@ -17,4 +18,10 @@ class PostCreate(CreateView):
 	model=Post
 	template_name='blog/post-create.html'
 	success_url=reverse_lazy('post-list')
-	fields=['title','author','text']
+	fields=['title','text']
+
+	def form_valid(self,form):
+		object=form.save(commit=False)
+		object.author=self.request.user
+		object.save()
+		return HttpResponseRedirect(self.success_url)
